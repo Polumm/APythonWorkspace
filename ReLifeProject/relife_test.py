3333a958957403fc637f4b586fc745255df91b86
Polumm <songchujiashenzhen@163.com>
2022-01-16 21:30:55 +0800

    1

---
+++ ReLifeProject/relife_test.py
@@ -0,0 +1,28 @@
+""""
+Author : Polumm
+Version: 0.1
+"""
+
+# 变量及输出
+print("hello world!")
+a = 100
+b = 12.345
+print(a + b)
+# a = input("a = ")
+print(a)
+
+# 条件循环语句
+results = 59
+if results >= 60:
+    print("及格")
+else:   # 注意Python严格缩进
+    print("不及格")
+
+test = 0
+if test:
+    print("1")
+    # test = 1
+test = 1    # 注意：这里没有花括号，严格按照缩进体现代码块
+if test:
+    print("1")
+
