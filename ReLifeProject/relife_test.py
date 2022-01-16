e67ecd3e5c8ed5665eb944f62296a84b55ecd42f
Polumm <songchujiashenzhen@163.com>
2022-01-16 22:31:27 +0800

    2021/1/16

--- ReLifeProject/relife_test.py
+++ ReLifeProject/relife_test.py
@@ -23,6 +23,16 @@ if test:
     print("1")
     # test = 1
 test = 1    # 注意：这里没有花括号，严格按照缩进体现代码块
-if test:
+if test:    # 非零非空可以像C++一样，直接代表true，但是没有(！变量)这样的写法
     print("1")
 
+# 多分支条件
+results = 89
+if results > 90:
+    print("优秀")
+elif results > 80:
+    print("良好")
+elif results > 60:
+    print("及格")
+else:
+    print("不及格")
